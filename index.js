e24dc01a3a6087216fc7a2bc2d7e04cc9eca4c94
georgegyan <georgegyankwame72@gmail.com>
2025-05-10 15:07:47 -0700

    Arithmetic Operators

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-console.log('Hello');
-console.log('I like pizza');
+// console.log('Hello');
+// console.log('I like pizza');
 
-window.alert('This is an alert!');
-window.alert('I like pizza!');
+// window.alert('This is an alert!');
+// window.alert('I like pizza!');
 
-document.getElementById("come").textContent = 'Hello';
-document.getElementById("si").textContent = 'You';
+// document.getElementById("come").textContent = 'Hello';
+// document.getElementById("si").textContent = 'You';
 
-// Variables
+// // Variables
+// let age = 25;
+// let price = 10.4;
+
+// console.log('Your age is ${age} years old');
+// console.log('The price is ${price}');
+
+// // String
+// let firstName = "Geo";
+// let favoriteFood = "Yam";
+// let email = "geo@gmail.com";
+
+// console.log(typeof firstName);
+// console.log('Your name is ${firstName}');
+// console.log('Food is ${favoriteFood}');
+// console.log('Email is ${email}');
+
+// //  Boolean
+// let online = true;
+
+// console.log('Geo is online: $(online)');
+
+let fulName = "Geo";
 let age = 25;
-let price = 10.4;
+let isStudent = true;
 
-console.log('Your age is ${age} years old');
-console.log('The price is ${price}');
+document.getElementById("p1").textContent = `Your name is ${fulName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
 
-// String
-let firstName = "Geo";
-let favoriteFood = "Yam";
-let email = "geo@gmail.com";
+// arithmetic operators
+let students = 30;
 
-console.log(typeof firstName);
-console.log('Your name is ${firstName}');
-console.log('Food is ${favoriteFood}');
+// students = students + 1;
 
-//  Boolean
+
+console.log(students);
+
+// let result = 23;
+
+let result = 0.046875;
+
+console.log(result);
