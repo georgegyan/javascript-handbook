d92d2913daf657bff7a3199863729e47ab737263
georgegyan <georgegyankwame72@gmail.com>
2025-05-09 18:26:03 -0700

    JavaScript Course and Projects

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log('Hello');
+console.log('I like pizza');
+
+window.alert('This is an alert!');
+window.alert('I like pizza!');
+
+document.getElementById("come").textContent = 'Hello';
+document.getElementById("si").textContent = 'You';
+
+// Variables
+let age = 25;
+let price = 10.4;
+
+console.log('Your age is ${age} years old');
+console.log('The price is ${price}');
+
+// String
+let firstName = "Geo";
+let favoriteFood = "Yam";
+let email = "geo@gmail.com";
+
+console.log(typeof firstName);
+console.log('Your name is ${firstName}');
+console.log('Food is ${favoriteFood}');
+
+//  Boolean
